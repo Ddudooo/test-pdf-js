10c2c3d4569640129162a3b027c38babd80b0c63
Ddudooo <igoa.work@gmail.com>
2022-03-08 01:13:49 +0900

    폰트 추가 샘플

---
+++ src/sample.ts
@@ -0,0 +1,11 @@
+import { jsPDF } from 'jspdf'
+import callAddFont from './fonts/HY_HEADLINE_M-normal'
+const doc = new jsPDF('l', 'mm', [200, 102])
+
+jsPDF.API.events.push(['addFonts', callAddFont])
+console.log(doc.getFontList())
+doc.setFont('HY_HEADLINE_M')
+
+doc.text('안녕하세요', 25, 10)
+
+doc.save('sample.pdf')
